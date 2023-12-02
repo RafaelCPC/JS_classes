0f3845c91c5067e182a3001ff8aa5349008c0777
RafaelCPC <148368805+RafaelCPC@users.noreply.github.com>
2023-12-03 00:53:29 +0100

    Add files via upload

--- instanceof.js
+++ instanceof.js
@@ -20,25 +20,28 @@ this.radius = radius;
 }
 }
 class AreaCalculator {
-    static calculate (Square, Rectangle, Circle) {
-        if(Square.side === true) {
-            let area =side*side;
-            console.log(area);
-        } else if(Rectangle === true) {
-            let area= width*height;
-            console.log(area);
-        } else if (Circle === true){
-            let area= 3.14*(radius*radius);
-            console.log(area);
+    
+    static calculate (x) {
+        
+        if(x instanceof Square) {
+           return square.side * square.side;
+        
+        } else if(x instanceof Rectangle) {
+          return rectangle.width * rectangle.height;
+            
+        } else if (x instanceof Circle) {
+           return 2 * Math.PI * circle.radius;
+        
+        } else{
+            console.log("not a rectangle,circle or square");
         }
-        return calculate(Square, Rectangle, Circle);
-    }    
+        
+    }   
 }
 
 const square = new Square(4);
 const rectangle = new Rectangle(4, 2);
 const circle = new Circle(5);
-
 console.log(AreaCalculator.calculate(square));
 console.log(AreaCalculator.calculate(rectangle));
 console.log(AreaCalculator.calculate(circle));
